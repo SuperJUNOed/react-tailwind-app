080405463b4c2170eb5271ead7a54366419bd728
SuperJUNOed <superjuno1018@gmail.com>
2023-05-03 05:32:49 +0900

    [Create] backend

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { loginFields } from "../constants/formFields";
+import FormAction from "./FormAction";
+import FormExtra from "./FormExtra";
+import Input from "./Input";
+
+const fields = loginFields;
+let fieldsState = {};
+fields.forEach((field) => (fieldsState[field.id] = ""));
+
+export default function Login() {
+  const [loginState, setLoginState] = useState(fieldsState);
+
+  const handleChange = (e) => {
+    setLoginState({ ...loginState, [e.target.id]: e.target.value });
+  };
+
+  //Handler to authenticate user on form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = loginState; // Destructuring email & password from loginState object
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    if (response.ok) {
+      window.location.href = '/dashboard';
+    } else {
+      const data = await response.json();
+      alert(data.message);
+    }
+  };
+
+  return (
+    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+      <div className="-space-y-px">
+        {fields.map((field) => (
+          <Input
+            key={field.id}
+            handleChange={handleChange}
+            value={loginState[field.id]}
+            labelText={field.labelText}
+            labelFor={field.labelFor}
+            id={field.id}
+            name={field.name}
+            type={field.type}
+            isRequired={field.isRequired}
+            placeholder={field.placeholder}
+          />
+        ))}
+      </div>
+
+      <FormExtra />
+      <FormAction handleSubmit={handleSubmit} text="Login" />
+    </form>
+  );
+}
